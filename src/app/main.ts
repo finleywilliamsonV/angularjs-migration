07dab9855c69b4d3445603ff4c11f7109647acec
Finley Williamson V <finley.williamson@gmail.com>
2020-09-03 10:06:57 -0400

    Remove Semicolons

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,24 +1,24 @@
-import 'angular';
-import 'angular-resource';
-import 'angular-animate';
-import 'ng-infinite-scroll';
-import 'angular-spinner';
-import 'angular-auto-validate/dist/jcs-auto-validate';
-import 'angular-ladda';
-import 'angular-strap';
-import 'angularjs-toaster';
-import 'angular-ui-router';
+import 'angular'
+import 'angular-resource'
+import 'angular-animate'
+import 'ng-infinite-scroll'
+import 'angular-spinner'
+import 'angular-auto-validate/dist/jcs-auto-validate'
+import 'angular-ladda'
+import 'angular-strap'
+import 'angularjs-toaster'
+import 'angular-ui-router'
 
-import './app.main';
-import './services';
-import './components';
-import './filters';
-import './app.routes';
+import './app.main'
+import './services'
+import './components'
+import './filters'
+import './app.routes'
 
-import './polyfills.ts';
+import './polyfills.ts'
 
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faMapMarker, faGift, faEnvelope, faPencilAlt, faTrash, faFemale, faMale } from "@fortawesome/free-solid-svg-icons";
+import { library, dom } from "@fortawesome/fontawesome-svg-core"
+import { faMapMarker, faGift, faEnvelope, faPencilAlt, faTrash, faFemale, faMale } from "@fortawesome/free-solid-svg-icons"
 
-library.add( faMale, faFemale, faMapMarker, faGift, faEnvelope, faPencilAlt, faTrash );
-dom.watch();
+library.add( faMale, faFemale, faMapMarker, faGift, faEnvelope, faPencilAlt, faTrash )
+dom.watch()
